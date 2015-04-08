08fdbb385366527eb4ec23dfb59d5207c77e0a56
Bernhard Molz <molzb2@gmx.de>
2015-04-08 14:40:52 +0200

    Holidays

--- web/js/holidaysController.js
+++ web/js/holidaysController.js
@@ -1,31 +1,170 @@
-var app = angular.module('TrionaModule', []);
+"use strict";
+var holidayApp = angular.module('holidayApp', []);
+var myScope;
+var DAY_IN_MS = 1000 * 60 * 60 * 24;
 
-app.controller('EmployeeController', ['$http', '$log', function($http, $log) {
-		var ctrl = this;
-		
-		ctrl.employees = [];
-		ctrl.projects = [];
-		ctrl.holidays = [];
+holidayApp.controller('holidayCtrl', function ($scope, $http) {
+	myScope = $scope;
+	$scope.year = new Date().getFullYear();
+	$scope.today = new Date();
+	$scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-		ctrl.searchTerm = '';
-		ctrl.orderBy = '';
-		ctrl.newEmployee = {};
+	$scope.from = null, $scope.to = null;
+	$scope.workingDays = 0;
+	$scope.remainingDays = 0;
+	$scope.takenDays = 0;
+	$scope.fromDate = null;
+	$scope.toDate = null;
+	$scope.myHolidays = [];
 
-		this.hello = "World";
+	$scope.me = {};
 
-		$http.get('GetServlet?type=employees').success(function(data) {
-			ctrl.employees = data;
-		}).error(function() {
-			console.log("FAIL");
+	$http.get('GetServlet?type=employees&id=4').success(function (data) {
+		$scope.me = data[0];
+
+		$http.get('GetServlet?type=holidays&id=4').success(function (data) {
+			$scope.myHolidays = data;
+			$scope.countTakenDays();
+
+			for (var i = 0; i < $scope.myHolidays.length; i++) {
+				var fromTokens = $scope.myHolidays[i].from.split("-");
+				var toTokens   = $scope.myHolidays[i].to.split("-");
+				var fromDate = new Date(fromTokens[0], fromTokens[1]-1, fromTokens[2], 0, 0, 0, 0);
+				var toDate   = new Date(toTokens[0], toTokens[1]-1, toTokens[2], 0, 0, 0, 0);
+				$scope.markDates(fromDate, toDate, true);
+			}
+		}).error(function () {
+			console.log("FAIL getHolidays");
 		});
 
-		this.search = function(searchT) {
-			console.log(searchT);
-			this.searchTerm = searchT;
-		};
+	}).error(function () {
+		console.log("FAIL getEmployee");
+	});
 
-		this.addEmployee = function() {
-			console.log("addEmployee");
-		};
-	}
-]);
+	$scope.chgYear = function (chg) {
+		this.year += chg;
+		this.initYear();
+	};
+
+	$scope.countTakenDays = function () {
+		this.remainingDays = this.me.holiday2015;
+		for (var i = 0; i < this.myHolidays.length; i++) {
+			var workingDays = this.myHolidays[i].workingDays;
+			this.takenDays += workingDays;
+			this.remainingDays -= workingDays;
+		}
+		console.log("taken=" + this.takenDays + ", remaining=" + this.remainingDays);
+	};
+
+	$scope.updateFrom = function () {
+		this.from = $("#datepickerFrom input").val();
+		console.log("from=" + this.from);
+		this.calcDays();
+	};
+	$scope.updateTo = function () {
+		this.to = $("#datepickerTo input").val();
+		console.log("to=" + this.to);
+		this.calcDays();
+	};
+
+	$scope.calcDays = function () {
+		if (this.from === null || this.to === null)
+			return;
+		this.workingDays = 0;
+		this.remainingDays = this.me.holiday2015 - this.takenDays;
+		var fromTokens = this.from.split(".");
+		var toTokens = this.to.split(".");
+		this.fromDate = new Date(fromTokens[2], fromTokens[1] - 1, fromTokens[0], 0, 0, 0, 0);
+		this.toDate = new Date(toTokens[2], toTokens[1] - 1, toTokens[0], 0, 0, 0, 0);
+		this.workingDays = this.markDates(this.fromDate, this.toDate, false);
+		this.remainingDays -= this.workingDays;
+	};
+
+	$scope.markDates = function (fromDate, toDate, isTaken) {
+		var workingDays = 0;
+		while (toDate.getTime() >= fromDate.getTime()) {
+			if (toDate.getDay() >= 1 && toDate.getDay() <= 5) {
+				workingDays++;
+				this.markDate(toDate.getFullYear(), toDate.getMonth(), toDate.getDate(), isTaken);
+			}
+			toDate.setTime(toDate.getTime() - DAY_IN_MS);	// set to previous day
+		}
+		return workingDays;
+	};
+
+	$scope.markDate = function (year, month, day, isTaken) {
+		if (year !== this.year)
+			return;
+
+		var tblOfMonth = $("#" + $scope.months[month] + " tbody");
+		var tdOfMonth = tblOfMonth.find("td").not(".outOfMth,.red");
+		console.log("mark " + day + "." + $scope.months[month] + " " + year + ", tdOfMonth len=" + tdOfMonth.length);
+		tdOfMonth.each(function () {
+			if ($(this)[0].innerHTML == day) {
+				$(this).addClass(isTaken ? "btn-success" : "btn-danger");
+			}
+		});
+	};
+
+	$scope.initMonth = function (monthNumber) {
+		var first = new Date($scope.year, monthNumber, 1, 0, 0, 0, 0);
+		var firstOfNextMth = new Date($scope.year, monthNumber + 1, 1, 0, 0, 0, 0);
+		var d = new Date(first.getTime());
+		while (d.getDay() !== 1) {	// monday
+			d.setTime(d.getTime() - DAY_IN_MS);
+		}
+		var id = $scope.months[monthNumber];
+		var tdsOfI = $("#" + id + " tbody td");
+		tdsOfI.each(function (i) {
+			if (i > 36)
+				return;	// it can't go any farther
+
+			var tdOfI = $(this);
+			if (d.getTime() < first.getTime()) {					// previous month
+				tdOfI.addClass("outOfMth").text(d.getDate());
+			} else if (d.getTime() < firstOfNextMth.getTime()) {	// current month
+				tdOfI.removeClass("outOfMth").text(d.getDate());
+			} else {												// next month
+				tdOfI.addClass("outOfMth").text(d.getDate());
+				if (i > 34)
+					tdOfI.html("&nbsp;");
+			}
+			if (tdOfI.hasClass("today"))
+				tdOfI.removeClass("today");
+			if (tdOfI.hasClass("btn-danger"))
+				tdOfI.removeClass("btn-danger");
+			if (tdOfI.hasClass("btn-success"))
+				tdOfI.removeClass("btn-success");
+
+			if (d.getFullYear() === $scope.today.getFullYear() &&
+					d.getMonth() === $scope.today.getMonth() &&
+					d.getDate() === $scope.today.getDate() &&
+					!tdOfI.hasClass("outOfMth")) {
+				tdOfI.addClass("today");
+			}
+			d.setTime(d.getTime() + DAY_IN_MS);
+		});
+	};
+
+	$scope.initYear = function () {
+		var t1 = window.performance.now();
+		for (var i = 0; i < 12; i++)
+			this.initMonth(i);
+		console.log("initYear took " + (window.performance.now() - t1) + " ms");
+	};
+
+	$scope.gotoToday = function () {
+		$scope.year = new Date().getFullYear();
+		$scope.initYear();
+	};
+
+	$scope.reset = function () {
+		$scope.from = "", $scope.to = "";
+	};
+
+	$(document).ready(function () {
+		$scope.initYear();
+		var options = {locale: "de", format: "DD.MM.YYYY", minDate: new Date()};
+		$("#datepickerFrom, #datepickerTo").datetimepicker(options);
+	});
+});
