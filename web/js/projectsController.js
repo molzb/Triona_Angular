9fe3030676d6a9b4a48f277690264e9339a8bae8
Bernhard Molz <molzb2@gmx.de>
2015-08-05 22:23:10 +0200

    Update README.md
    Removing unused files

--- web/js/projectsController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-routeApp.controller('ProjectsCtrl', function (MyService) {
-	var ctrl = this;
-	ctrl.projects = [];
-
-	MyService.loadProjects().then(function (d) {
-		ctrl.projects = MyService.getProjects();
-	});
-});
